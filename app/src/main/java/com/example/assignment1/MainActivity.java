a843708b421524fbeade7b8d4f2bb8a32baa0527
lilyraf <95680925+lilyraf@users.noreply.github.com>
2023-06-12 01:34:01 +0800

    newest

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -32,16 +33,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUnit = (EditText) findViewById(R.id.etUnit);
-        etRebate = (EditText) findViewById(R.id.etRebate);
-        btnConvert = (Button) findViewById(R.id.btnConvert);
-        btnClear = (Button) findViewById(R.id.btnClear);
-        tvCalcDet = (TextView) findViewById(R.id.tvCalcDet);
-        tvTcharge = (TextView) findViewById(R.id.tvTcharge);
-        tvFcharge = (TextView) findViewById(R.id.tvFcharge);
-        tvFcharge2 = (TextView) findViewById(R.id.tvFcharge2);
-        tvRebate = (TextView) findViewById(R.id.tvRebate);
-        tvBlock = (TextView) findViewById(R.id.tvBlock);
+        etUnit = findViewById(R.id.etUnit);
+        etRebate = findViewById(R.id.etRebate);
+        btnConvert = findViewById(R.id.btnConvert);
+        btnClear = findViewById(R.id.btnClear);
+        tvCalcDet = findViewById(R.id.tvCalcDet);
+        tvTcharge = findViewById(R.id.tvTcharge);
+        tvFcharge = findViewById(R.id.tvFcharge);
+        tvFcharge2 = findViewById(R.id.tvFcharge2);
+        tvRebate = findViewById(R.id.tvRebate);
+        tvBlock = findViewById(R.id.tvBlock);
         btnConvert.setOnClickListener(this);
         btnClear.setOnClickListener(this);
 
@@ -55,109 +56,116 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch(v.getId()){
             case R.id.btnConvert:
-                try {
-                    double meter = Double.parseDouble(etUnit.getText().toString());
-                    double rebate = 0.0; // Default value of 0
-                    String rebateText = etRebate.getText().toString();
-                    if (!rebateText.isEmpty()) {
-                        rebate = Double.parseDouble(rebateText) ;
-                        if (rebate > 5){
+                calcCharges();//call function to calculate
+                break;
+            case R.id.btnClear:
+                clearFields();//call function to clear
+                break;
+        }
+    }
+    @SuppressLint("DefaultLocale")
+    private void calcCharges(){
+        try {
+            double meter = Double.parseDouble(etUnit.getText().toString());
+            double rebate = 0.0; // Default value of 0
+            String rebateText = etRebate.getText().toString();
+            if (!rebateText.isEmpty()) {
+                rebate = Double.parseDouble(rebateText) ;
+                if (rebate > 5){
 
-                            throw new IllegalArgumentException();
-                        }else{
-                            rebate = rebate/100;
-                        }
+                    throw new IllegalArgumentException();
+                }else{
+                    rebate = rebate/100;
+                }
 
-                    }
-                    int B1 = 200; //block size
-                    int B2 = 100;
-                    int B3 = 300;
-                    double B1_rate = 0.218; // RM/kWh for first 200 kWh (1 - 200 kWh) per month
-                    double B2_rate = 0.334; // RM/kWh for next 100 kWh (201 - 300 kWh) per month
-                    double B3_rate = 0.516; // RM/kWh for next 300 kWh (301 - 600 kWh) per month
-                    double B4_rate = 0.546; // RM/kWh for next 300 kWh onwards (601-900++ kWh) per month
-                    double B1_cost = B1*B1_rate; // cost for full block
-                    double B2_cost = B2*B2_rate;
-                    double B3_cost = B3*B3_rate;
-                    double TotalCharge = 0.0;
-                    double Fcharge = 0.0;
-                    String calculation= "";
-                    String output = "";
+            }
+            int B1 = 200; //block size
+            int B2 = 100;
+            int B3 = 300;
+            double B1_rate = 0.218; // RM/kWh for first 200 kWh (1 - 200 kWh) per month
+            double B2_rate = 0.334; // RM/kWh for next 100 kWh (201 - 300 kWh) per month
+            double B3_rate = 0.516; // RM/kWh for next 300 kWh (301 - 600 kWh) per month
+            double B4_rate = 0.546; // RM/kWh for next 300 kWh onwards (601-900++ kWh) per month
+            double B1_cost = B1*B1_rate; // cost for full block
+            double B2_cost = B2*B2_rate;
+            double B3_cost = B3*B3_rate;
+            double TotalCharge;
+            double Fcharge = 0.0;
+            String calculation= "";
+            String output = "";
 
 
-                    if (meter > 600) {
-                        TotalCharge = B1_cost + B2_cost + B3_cost + meter%600*B4_rate;
-                        output = String.format("Total Charge: RM%.2f", TotalCharge);
-                        calculation = String.format(
-                                "Unit Used: %d kWh\n" +
+            if (meter > 600) {
+                TotalCharge = B1_cost + B2_cost + B3_cost + ((meter-600)*B4_rate);
+                output = String.format("Total Charge: RM%.2f", TotalCharge);
+                calculation = String.format(
+                        "Unit Used: %d kWh\n" +
                                 "Blocks (kWh):\n\n" +
                                 "First 200: 200*"+ B1_rate +" = RM" +B1_cost + "\n" +
                                 "Next 100: 100*"+ B2_rate +" = RM" +B2_cost+ "\n" +
                                 "Next 300: 300*"+ B3_rate +" = RM" +B3_cost+ "\n" +
-                                "Next 300: " + "%.2f*"+ B4_rate+ " = RM"+ meter%600*B4_rate +"\n"
-                                ,(int) meter, meter%600);
-                        Fcharge = TotalCharge - (TotalCharge * rebate);
-                    } else if (meter > 300) {
-                        TotalCharge =  B1_cost + B2_cost + (meter % 300) * B3_rate;
-                        calculation = String.format(
-                                "Unit Used: %d kWh\n" +
-                                        "Blocks (kWh):\n\n" +
-                                        "First 200: 200*"+B1_rate+" = RM" +B1_cost + "\n" +
-                                        "Next 100: 100*"+B2_rate+" = RM" +B2_cost+ "\n" +
-                                        "Next 300: " + "%.2f*"+B3_rate+" = RM" + (meter % 300) * B3_rate +"\n"
-                                ,(int) meter, meter%300);
-                        output = String.format("Total Charge: RM%.2f", TotalCharge);
-                        Fcharge = TotalCharge - (TotalCharge * rebate);
-                    } else if (meter > 200) {
-                        TotalCharge =  B1_cost + (meter % 200) * B2_rate;
-                        calculation = String.format(
-                                "Unit Used: %d kWh\n" +
+                                "Next 300: " + "%.2f*"+ B4_rate+ " = RM"+ ((meter-600)*B4_rate) +"\n"
+                        ,(int) meter, (meter-600));
+                Fcharge = TotalCharge - (TotalCharge * rebate);
+            } else if (meter > 300) {
+                TotalCharge =  B1_cost + B2_cost + (meter - 300) * B3_rate;
+                calculation = String.format(
+                        "Unit Used: %d kWh\n" +
                                 "Blocks (kWh):\n\n" +
                                 "First 200: 200*"+B1_rate+" = RM" +B1_cost + "\n" +
-                                "Next 100: " + "%.2f*"+B2_rate+" = RM" +(meter % 200) * B2_rate+"\n"
-                                ,(int) meter, meter%200);
-                        output = String.format("Total Charge: RM%.2f", TotalCharge);
-                        Fcharge = TotalCharge - (TotalCharge * rebate);
-                    } else if (meter > 0) {
-                        TotalCharge = meter * B1_rate;
-                        calculation = String.format(
+                                "Next 100: 100*"+B2_rate+" = RM" +B2_cost+ "\n" +
+                                "Next 300: " + "%.2f*"+B3_rate+" = RM" + (meter - 300) * B3_rate +"\n"
+                        ,(int) meter, (meter-300));
+                output = String.format("Total Charge: RM%.2f", TotalCharge);
+                Fcharge = TotalCharge - (TotalCharge * rebate);
+            } else if (meter > 200) {
+                TotalCharge =  B1_cost + (meter - 200) * B2_rate;
+                calculation = String.format(
+                        "Unit Used: %d kWh\n" +
+                                "Blocks (kWh):\n\n" +
+                                "First 200: 200*"+B1_rate+" = RM" +B1_cost + "\n" +
+                                "Next 100: " + "%.2f*"+B2_rate+" = RM" +(meter - 200) * B2_rate+"\n"
+                        ,(int) meter, (meter-200));
+                output = String.format("Total Charge: RM%.2f", TotalCharge);
+                Fcharge = TotalCharge - (TotalCharge * rebate);
+            } else if (meter > 0) {
+                TotalCharge = meter * B1_rate;
+                calculation = String.format(
                         "Unit Used: %d kWh\n" +
                                 "Blocks (kWh):\n" +
                                 "First 200: " + "%.2f*"+B1_rate+" = RM" + meter * B1_rate +"\n"
-                                ,(int) meter, meter);
-                        output = String.format("Total Charge: RM%.2f", TotalCharge);
-                        Fcharge = TotalCharge - (TotalCharge * rebate);
-                    }
-                    tvCalcDet.setText("\nCalculation Details");
-                    tvBlock.setText(calculation);
-                    tvRebate.setText(String.format("Rebate: %.1f%%", rebate*100));
-                    tvFcharge.setText(String.format("Final Charge: RM%.2f", Fcharge));
-                    tvFcharge2.setText(String.format("Final Charge: RM%.2f", Fcharge));
-                    tvTcharge.setText(output);
-                    Toast.makeText(this, "Calculation is completed!", Toast.LENGTH_SHORT).show();
-                    } catch (NumberFormatException e) {
-                    // Handle the exception when the input is not a valid number
-                    Toast.makeText(this, "Please enter electricity unit used!", Toast.LENGTH_SHORT).show();
-                } catch (IllegalArgumentException e){
-                    Toast.makeText(this, "Rebate only 0-5% !", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.btnClear:
-                tvCalcDet.setText("");
-                tvTcharge.setText("");
-                tvFcharge.setText("");
-                tvFcharge2.setText("");
-                tvBlock.setText("");
-                tvRebate.setText("");
-                etUnit.setText("");
-                etRebate.setText("");
-                Toast toast = Toast.makeText(this, "Cleared!", Toast.LENGTH_SHORT);
-                toast.setDuration(250);
-                toast.show();
-                break;
+                        ,(int) meter, meter);
+                output = String.format("Total Charge: RM%.2f", TotalCharge);
+                Fcharge = TotalCharge - (TotalCharge * rebate);
+            }
+            tvCalcDet.setText("\nCalculation Details");
+            tvBlock.setText(calculation);
+            tvRebate.setText(String.format("Rebate: %.1f%%", rebate*100));
+            tvFcharge.setText(String.format("Final Charge: RM%.2f", Fcharge));
+            tvFcharge2.setText(String.format("Final Charge: RM%.2f", Fcharge));
+            tvTcharge.setText(output);
+            Toast.makeText(this, "Calculation is completed!", Toast.LENGTH_SHORT).show();
+        } catch (NumberFormatException e) {
+            // Handle the exception when the input is not a valid number
+            Toast.makeText(this, "Please enter electricity unit used!", Toast.LENGTH_SHORT).show();
+        } catch (IllegalArgumentException e){
+            // Handle the exception when the input is bigger than 5
+            Toast.makeText(this, "Rebate only 0-5% !", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void clearFields(){
+        tvCalcDet.setText("");
+        tvTcharge.setText("");
+        tvFcharge.setText("");
+        tvFcharge2.setText("");
+        tvBlock.setText("");
+        tvRebate.setText("");
+        etUnit.setText("");
+        etRebate.setText("");
+        Toast toast = Toast.makeText(this, "Cleared!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
@@ -168,7 +176,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.about :
-            //Toast.makeText(this,"This is about",Toast.LENGTH_LONG).show();
             Intent intent = new Intent(this, AboutActivity.class) ;
             startActivity(intent);
                 break;
